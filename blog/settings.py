96d79f04283403bbaf5d692fcda06b29d63b2712
Y-Tarek <yassertarek98@gmail.com>
2024-06-21 15:41:25 +0300

    allow cors

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ MEDIA_URL = '/media/'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+CORS_ORIGIN_ALLOW_ALL = True  
 
 # Application definition
 
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'corsheaders',
     'users',
     'app',
 ]
@@ -59,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
